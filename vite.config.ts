3d0ec8152bb1bceb1674e7f61e9c0614d56f3a44
Allen Cao <46756655+AllenCaoo@users.noreply.github.com>
2024-07-05 13:23:39 -0700

    Trying different slash

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/3D-React-Portfolio"
+  base: "/3D-React-Portfolio/"
 })
