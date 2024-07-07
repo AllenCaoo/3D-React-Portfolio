7f5a90a5f151f421287d7d89e3c5ee3f734b5e8b
Allen Cao <46756655+AllenCaoo@users.noreply.github.com>
2024-07-06 22:23:21 -0700

    Added texture for bookshelf

--- src/components/Bookshelf/Bookshelf.tsx
+++ src/components/Bookshelf/Bookshelf.tsx
@@ -1,5 +1,6 @@
 import '../../index.css'
 import Books from './Books';
+import { useTexture } from '@react-three/drei';
 
 
 
@@ -14,35 +15,39 @@ const Bookshelf = ({
     position: [number, number, number];
 }) => {
 
+  const bookshelfTexture = useTexture({map: 'textures/bookshelf.png'})
+
   return (
         <>
             <mesh position={position}>
               <mesh>
                 <boxGeometry args={[10, 0.1, 1.8]}/>
-                <meshLambertMaterial/>
+                <meshLambertMaterial {...bookshelfTexture}/>
               </mesh>
               <mesh position={[0, -2.9, 0]}>
                 <boxGeometry args={[10, 0.1, 1.8]}/>
-                <meshLambertMaterial/>
+                <meshLambertMaterial {...bookshelfTexture}/>
               </mesh>
               <mesh position={[0, -5.8, 0]}>
                 <boxGeometry args={[10, 0.1, 1.8]}/>
-                <meshLambertMaterial/>
+                <meshLambertMaterial {...bookshelfTexture}/>
               </mesh>
               <mesh position={[0, -2.9, 0]}>
                 <boxGeometry args={[0.1, 5.8, 1.8]}/>
-                <meshLambertMaterial/>
+                <meshLambertMaterial {...bookshelfTexture}/>
               </mesh>
               <mesh position={[5, -2.9, 0]}>
                 <boxGeometry args={[0.1, 5.8, 1.8]}/>
-                <meshLambertMaterial/>
+                <meshLambertMaterial {...bookshelfTexture}/>
               </mesh>
               <mesh position={[-5, -2.9, 0]}>
                 <boxGeometry args={[0.1, 5.8, 1.8]}/>
-                <meshLambertMaterial/>
+                <meshLambertMaterial {...bookshelfTexture}/>
               </mesh>
               <Books position={[-5 + 0.5,-(2.5/2 + 0.3),0]}/>
+              <Books position={[-5 + 0.5, -4.5, 0]}/>
               <Books position={[0.5,-(2.5/2 + 0.3),0]}/>
+              <Books position={[0.5,-4.5,0]}/>
             </mesh>
         </>
   );
