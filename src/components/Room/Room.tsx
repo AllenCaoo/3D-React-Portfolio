c2341fbf0381fd2e8875465015acefe73c36beaf
Allen Cao <46756655+AllenCaoo@users.noreply.github.com>
2024-07-06 23:06:49 -0700

    Added couch

--- src/components/Room/Room.tsx
+++ src/components/Room/Room.tsx
@@ -1,6 +1,7 @@
 import '../../index.css'
 import Bookshelf from '../Bookshelf/Bookshelf';
 import Wall from './Wall';
+import { useFBX } from '@react-three/drei';
 
 
 
@@ -9,6 +10,9 @@ const Room = ({
 }: {
     position: [number, number, number];
 }) => {
+
+  const L_couch = useFBX('models/L_couch.fbx');
+
   return (
         <>
 
@@ -26,6 +30,8 @@ const Room = ({
             <Wall position={[position[0], position[1] - 11 + 30/2, position[1] + 30/2]} dimensions={[25, 20, 0.1]}/>
             <Wall position={[position[0], position[1] - 11 + 30/2, position[1] - 30/2]} dimensions={[25, 20, 0.1]}/>
             <Wall position={[position[0]-25/2, position[1] - 11 + 30/2, position[1]]} dimensions={[0.1, 20, 30]}/>
+
+            <primitive object={L_couch} scale={0.015} rotation={[0,Math.PI/2,0]} position={[position[0]-10, position[1] - 6, position[1] - 6]} />
 
 
             <Bookshelf position={[position[0], position[1], position[2] - 13.5]}/>
