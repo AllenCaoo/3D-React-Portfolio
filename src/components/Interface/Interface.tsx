a146fa7f022cb801e789cdc668cf14fb4e9d7664
Allen Cao <46756655+AllenCaoo@users.noreply.github.com>
2024-07-18 00:37:10 -0700

    Enabling library view

--- src/components/Interface/Interface.tsx
+++ src/components/Interface/Interface.tsx
@@ -1,29 +1,46 @@
+import { Canvas, useFrame } from '@react-three/fiber';
 import '../../App.css'
 import NavButton from '../Menu/NavButton';
 import Scene from '../Scene/Scene';
 import { useState } from 'react';
+import { Vector3 } from 'three';
 
 const Interface = () => {
 
-  // useThree(({ camera }) => {
-  //   camera.rotation.set(1, 0, 0);
-  // });
+  const [inLibraryView, setIsLibraryView] = useState(false);
 
-  const [camPosition, setCamPosition] = useState([1, 1, 1]);
-//   const [inLibraryView, setIsLibraryView] = useState(false)
+  const INITAL_POSITION = new Vector3(10, 0, 12)
 
 
   const setLibraryView = () => {
-    setCamPosition([10,0,0])
-    // setIsLibraryView(true)
+    setIsLibraryView(!inLibraryView)
+  }
+
+  const CameraRig = () => {
+    useFrame((state) => {
+      if (inLibraryView) {
+        let vec = new Vector3(0, -0.5, -5)
+        state.camera.position.lerp(vec, 0.03)
+        state.camera.lookAt(0, 0, -40)
+      } else {
+        state.camera.position.lerp(INITAL_POSITION, 0.03)
+      }
+      state.camera.lookAt(0, 0, -40)
+    })
+  
+    return null
   }
 
   return (
     <>
       <NavButton onClick={setLibraryView}/>
-      <Scene camPosition={camPosition}/>
+      <Canvas>
+        <CameraRig />  {/* Comment this out for free move debugging */}
+        <Scene />
+      </Canvas>
     </>
   );
 }
+
 
 export default Interface
--- src/components/Scene/Scene.tsx
+++ src/components/Scene/Scene.tsx
@@ -1,47 +1,23 @@
-import { Canvas, useFrame } from '@react-three/fiber'
 import { OrbitControls, ScrollControls } from "@react-three/drei";
 import '../../App.css'
 import Room from '../Room/Room';
-import { Vector3 } from 'three';
 
-const Scene = ({
-  camPosition
-} : {
-  camPosition: any;
-}) => {
-
-  // useThree(({ camera }) => {
-  //   camera.rotation.set(1, 0, 0);
-  // });
-
-  console.log(camPosition)
+const Scene = () => {
 
   return (
-      <Canvas 
-        camera={{
-          position: camPosition
-        }}
-        >
-        <CameraRig position={camPosition} />
         <>
           <ambientLight intensity={1} />
-          <OrbitControls enableZoom={true} />
+          <OrbitControls 
+              enableZoom={true} 
+              minAzimuthAngle={-Math.PI/4}
+              maxAzimuthAngle={Math.PI/4} 
+              /* TODO:  Restrict vertical angling */
+              />
           <ScrollControls pages={3} damping={0.25}>
             <Room position={[0,0,0]}/>
           </ScrollControls>
         </>
-      </Canvas>
   );
-}
-
-const CameraRig = (position : any) => {
-  useFrame((state) => {
-    let vec = new Vector3(position[0], position[1], position[2])
-    state.camera.position.lerp(vec, 0.1)
-    // state.camera.lookAt(10, 10, 10)
-  })
-
-  return null
 }
 
 export default Scene
