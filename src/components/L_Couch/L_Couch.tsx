af780d1796452441c720ba2dd8e5f96b2d3cb74b
Allen Cao <allencao@Allens-MacBook-Air.local>
2025-12-26 18:59:13 -0800

    Made couch less shiny

--- src/components/L_Couch/L_Couch.tsx
+++ src/components/L_Couch/L_Couch.tsx
@@ -1,5 +1,6 @@
 import '../../index.css';
 import { useFBX } from '@react-three/drei';
+import { useEffect } from 'react';
 
 
 
@@ -15,6 +16,25 @@ const L_Couch =  ({
 }) => {
 
   const scene = useFBX('models/L_couch.fbx');
+
+  useEffect(() => {
+    scene.traverse((child: any) => {
+      if (child.isMesh && child.material) {
+        console.log("child is mesh and has material");
+        console.log(child);
+        // Normalize to array (if it's already an array, keep it, otherwise wrap)
+        const materials = Array.isArray(child.material) ? child.material : [child.material];
+
+        materials.forEach((mat: any) => {
+          if (mat.isMeshPhongMaterial) {
+            mat.shininess = 2; // very low shininess
+            // mat.specular.set(0x111111); // dark specular highlight
+            mat.needsUpdate = true;  // Ensure the material updates
+          }
+        });
+      }
+    });
+  }, [scene]);
 
   return (
     <primitive object={scene} scale={scale} rotation={rotation} position={position} />
